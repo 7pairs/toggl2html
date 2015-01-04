d8373eeb603b61793fbe6133fbaa6d9c628a6665
Jun-ya HASEBA <7pairs@gmail.com>
2015-01-04 17:33:20 +0900

    Change the command line arguments #15

--- toggl2html/toggl2html.py
+++ toggl2html/toggl2html.py
@@ -1,45 +1,26 @@
 # -*- coding: utf-8 -*-
 
-"""
-Tools for converting Toggl CSV to HTML
-
-Usage:
-toggl2html -i <input> -o <output>
-
-Options:
--i <input>    input file path (CSV).
--o <output>   output file path (HTML).
--h --help     print this help message and exit.
--v --version  print the version number and exit.
-"""
-import os
-
-from docopt import docopt
+import sys
 
 try:
     # setup.py実行後
     from toggl2html.converter import detail
-    from toggl2html import __version__
 except ImportError:
     # 未インストール状態での動作確認時
     from converter import detail
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    __version__ = [
-        line.split('=')[1].strip().replace("'", '')
-        for line in open(os.path.join(dir_path, '__init__.py'))
-        if line.startswith('__version__ = ')
-    ][0]
 
 
 def main():
     """
     CSVファイルをHTMLに変換する。
     """
-    # 引数を取得する
-    args = docopt(__doc__, version=__version__)
+    # 引数をチェックする
+    if len(sys.argv) != 3:
+        print('Usage: toggl2html <input-file-path> <output-file-path>')
+        quit()
 
-    # CSVファイルをHTMLに変換する
-    detail.execute(args['-i'], args['-o'])
+    # 変換を実行する
+    detail.execute(sys.argv[1], sys.argv[2])
 
 
 if __name__ == '__main__':
